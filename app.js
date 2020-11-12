04e003fb4851e515c5caac503c0b497e5caf912f
Gabriel <gabrielacevedo.0629@gmail.com>
2020-11-12 08:00:45 -0500

    uso de etiquetas h1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send(' El código secreto es HcUy6Re2LLBRtj');
+    res.send('<h1>El código secreto es HcUy6Re2LLBRtj</h1>');
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
